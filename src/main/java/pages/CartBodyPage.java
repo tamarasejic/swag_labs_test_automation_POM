34f48b8c64c731b45b14cfd8e781c47e865b5b89
Tamara Sejic <sejic.tamara@gmail.com>
2023-10-14 04:29:21 +0200

    Verify if the items added is presented - TC_IL_00

--- src/main/java/pages/CartBodyPage.java
+++ src/main/java/pages/CartBodyPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -18,6 +19,19 @@ public class CartBodyPage extends BasicPage{
 
     public int numberOfItemsAdded(){
         return cartItemsAdded().size();
+    }
+
+    public WebElement getProductInCartByProductNameText(String productName) {
+        String selector = "//*[text()='" + productName + "']/../../..";
+        return driver.findElement(By.xpath(selector));
+    }
+
+    public boolean doesProductInCartExistByProductNameText(String productName){
+        wait
+                .withMessage(productName + " is not visible.")
+                .until(ExpectedConditions.visibilityOf
+                        (getProductInCartByProductNameText(productName)));
+        return true;
     }
 
 }
--- src/test/java/tests/CartTests.java
+++ src/test/java/tests/CartTests.java
@@ -367,4 +367,25 @@ public class CartTests extends BasicTest{
                 "It should close the left menu navigation.");
     }
 
+    @Test (retryAnalyzer = SwagLabsRetry.class)
+    public void verifyIfTheItemsAddedIsPresented() {
+        String username = "standard_user";
+        String password = "secret_sauce";
+
+        loginPage.clearAndTypeUsername(username);
+        loginPage.clearAndTypePassword(password);
+        loginPage.clickOnLoginButton();
+        urlPage.waitForCurrentPageToBeInventoryPage();
+
+        inventoryPage.scrollToProductName("Sauce Labs Backpack");
+        inventoryPage.clickOnAddToCartButtonByProductName("Sauce Labs Backpack");
+
+        topNavPage.clickOnCartButton();
+        urlPage.waitForCurrentPageToBeCartPage();
+
+        Assert.assertTrue(cartBodyPage
+                        .doesProductInCartExistByProductNameText("Sauce Labs Backpack"),
+                "Added item should be visible in the cart page");
+    }
+
 }
